91bc8e9eb14240101f162956ee5118f93bc7eba9
Jackson Calhoun <jwcalh19@harrisonburg.k12.va.us>
2019-09-17 09:12:56 -0400

    hWQIU

--- game.js
+++ game.js
@@ -2,13 +2,17 @@ var score = 0;
 
 postToPage(score);
 
-createButton('Button', btnPress);
+createButton('Button +', btnPress);
+createButton('Button -', btnPressOther);
 changeTitle('Clicker Game');
 
 setInterval(btnPress, 1000);
 
 function btnPress() {
-  score++;
+  score = score + 5;
   postToPage(score);
 }
-
+function btnPressOther() {
+  score--;
+  postToPage(score);
+}
