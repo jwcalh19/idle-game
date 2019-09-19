284f87ab205fac8650fff514dd1f2a1d25ae13b8
Jackson Calhoun <jwcalh19@harrisonburg.k12.va.us>
2019-09-19 09:43:59 -0400

    idle game

--- game.js
+++ game.js
@@ -1,11 +1,13 @@
 var score = 1; // Create a variable to store the score in
+                 
+let button = new Button('BOOM!'); 
+button.hide()
 
 postToPage(score);
-
 createButton('Button +', btnPress);
 createButton('Button -', btnPressOther);
-changeTitle('Clicker Game');
 
+setInterval(scoreIncrease, 1000);
 setInterval(btnPress, 1000);
 
 function btnPress() {
@@ -14,5 +16,16 @@ function btnPress() {
 }
 function btnPressOther() {
   score--;
-  postToPage(score);
+  postToPage(score);                 
+}
+function button2() {
+  score = 0;
+  
+}
+function scoreIncrease() {
+  score++;                       
+
+  if(score > 10) {
+    button.show();               
+  }
 }
