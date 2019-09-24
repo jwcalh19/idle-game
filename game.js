fbaaf3affa4d57c1edc9569ae8451ff3952ecdc0
Jackson Calhoun <jwcalh19@harrisonburg.k12.va.us>
2019-09-24 09:46:27 -0400

    Update game.js

--- game.js
+++ game.js
@@ -5,8 +5,10 @@ var hometype = 'None';
 let home = new Text('Home = ' + hometype);
 let heading = new HTMLElement('h3', "Wilton's Money");
 var button = new Button('Money +', btnPress);         
-var upgradebutton = new Button('Upgrade Home');
+var upgradebutton = new Button('Upgrade Home', upgradepress);
 upgradebutton.hide()
+var duckbutton = new Button('Buy A Duck')
+duckbutton.hide()
 var scr = new Text(score);                     
 
 setInterval(btnPress, 1000);   
@@ -14,9 +16,25 @@ setInterval(btnPress, 1000);
 function btnPress() {
   score++;         
   scr.edit(score); 
+  buyduck();
+  buyhome();
+ 
 }
-function upgradehome(){
-if (score > 50) {
+function buyhome(){
+if (score > 49) {
   upgradebutton.show();
+} else{
+  upgradebutton.hide();
 }
 }
+function buyduck(){
+if (score > 99) {
+  duckbutton.show();
+} else{
+  duckbutton.hide();
+}
+}
+function upgradepress(){
+  home.edit('Home = Apartment')
+
+ }
